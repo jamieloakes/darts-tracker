eefe5f3debeda4a41721df4267a2bad2389bda39
jamieloakes <jamieloakeswork@gmail.com>
2025-02-26 15:25:19 +0000

    Added main options menu function

--- src/menus.py
+++ src/menus.py
@@ -1,7 +1,21 @@
-import sqlite3
-import utils
+import src.utils as utils 
 
-def singlesRTWMenu() -> None:
+def printOptionMenu() -> None:
+    """
+        Print option screen for practice games
+    """
+    print('Select a practice game:')
+    print('-------------------------------------')
+    print('1: Round the World (Singles)')
+    print('2: Round the World (Doubles)')
+    print('3: 501')
+    print('0: Quit')
+
+
+def singlesRtwMenu() -> None:
+    """
+        Data capture menu for Round the World (Singles) practice game
+    """
     today:str = utils.getTodaysDate()
     data:list[tuple[str, str, int]] = [] 
     for i in range(1,21):
@@ -9,4 +23,4 @@ def singlesRTWMenu() -> None:
         data.append((today, f'S{i}', attempts))
 
 if __name__ == '__main__':
-    singlesRTWMenu()
+    singlesRtwMenu()
