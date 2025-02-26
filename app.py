5a06d73d1beaab64ac2bd756c8143d0bb19a0f82
jamieloakes <jamieloakeswork@gmail.com>
2025-02-26 14:03:20 +0000

    Set-up database creation script

---
+++ app.py
@@ -0,0 +1,4 @@
+from cfg import db_setup
+
+if __name__ == '__main__':
+    db_setup.setupDatabase()
---
+++ cfg/db_setup.py
@@ -0,0 +1,53 @@
+import sqlite3
+import os
+
+def setupDatabase() -> None:
+    """
+        Create database with required tables and fields\n
+        See docs/ folder for more info on schema and fields
+    """
+    if os.path.exists('practice_data.db'):
+        return
+
+    conn:sqlite3.Connection = sqlite3.connect('practice_data.db')
+    cursor:sqlite3.Cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS singles_rtw (
+        event_id INTEGER PRIMARY KEY,
+        event_date TEXT NOT NULL,
+        target TEXT NOT NULL,
+        attempts INTEGER NOT NULL
+    ); """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS doubles_rtw (
+        event_id INTEGER PRIMARY KEY,
+        event_date TEXT NOT NULL,
+        target TEXT NOT NULL,
+        hits INTEGER NOT NULL
+    ); """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS legs_stats (
+        leg_id INTEGER PRIMARY KEY,
+        event_date TEXT NOT NULL,
+        n_darts INTEGER NOT NULL,
+        avg REAL NOT NULL,
+        checkout_attempts INTEGER NOT NULL,
+        win INTEGER NOT NULL
+    ); """)
+
+    conn.close()
+
+
+def __checkTables() -> None:
+    conn:sqlite3.Connection = sqlite3.connect('practice_data.db')
+    cursor:sqlite3.Cursor = conn.cursor()
+    cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+    print(cursor.fetchall())
+
+
+if __name__ == '__main__':
+    setupDatabase()
+    __checkTables()
