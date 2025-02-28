7eb1270c7e7939b78f4ffee4e8291f2d45a8481f
jamieloakes <jamieloakeswork@gmail.com>
2025-02-28 19:02:44 +0000

    Added docstring

--- tests/db_query.py
+++ tests/db_query.py
@@ -3,12 +3,12 @@ import polars as pl
 
 
 def testDatabaseQuery() -> None:
+    """ Test function for querying database and extracting data """
     conn:sqlite3.Connection = sqlite3.connect('practice_data.db')
     query:str = """ SELECT * FROM legs_stats """
     
     df:pl.DataFrame = pl.read_database(query=query, connection=conn).with_columns(pl.col('event_date').str.to_date())
     print(df)
-    print(df.schema)
 
 
 if __name__ == '__main__':
