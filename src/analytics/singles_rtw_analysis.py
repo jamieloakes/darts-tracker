69eac1ed0997e243fb22b7c4797181a0983261ff
jamieloakes <jamieloakeswork@gmail.com>
2025-03-02 20:01:34 +0000

    Refactored visualisations functions

--- src/analytics/singles_rtw_analysis.py
+++ src/analytics/singles_rtw_analysis.py
@@ -1,5 +1,6 @@
 import polars as pl
 import matplotlib.pyplot as plt
+
 import src.database as database
 import modules.utils as utils
 
@@ -12,33 +13,39 @@ query:str = """
 
 
 def attemptsTable():
-    """
-        Show table view of total attempts per target
-    """
+    """ Create table view of total attempts per target and save as .png """
     df:pl.DataFrame = database.queryDatabase(query=query)
-    with pl.Config(tbl_rows=20):
-        print(df)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8,8)
+    ax.axis('off')
+
+    table = plt.table(cellText=df.to_pandas(), loc='center', cellLoc='center')
+    table.set_fontsize(size=16)
+    table.scale(1,2)
+
+    plt.savefig('./src/analytics/charts/singles_rtw_table.png')
 
 
 def dartboardHeatmap():
     """
-        Show dartboard and colour each target segment based on total attempts\n
-        Darker colour means more attempts at target
+        Create dartboard visualsation and and save as .png\n
+        More vibrant colour means more attempts at target\n
     """
     df:pl.DataFrame = database.queryDatabase(query=query)
 
     values:list[int] = [360 / 20 for i in range(20)]
     labels:list[str] = ['S20', 'S5', 'S12', 'S9', 'S14', 'S11', 'S8', 'S16', 'S7', 'S19',
                         'S3', 'S17', 'S2', 'S15', 'S10', 'S6', 'S13', 'S4', 'S18', 'S1']
-    colours:list[tuple[int,int,int]] = utils.conditionalFormatter(df=df, y_col='total_attempts', labels=labels)
+    colours:list[tuple[int,int,int]] = utils.conditionalFormatter(df=df, y_col='total_attempts', labels=labels, reverse=False)
     
     fig, ax = plt.subplots()
     fig.set_size_inches(12,8)
     # Set each segment to 18 as this is the area for each target on the dartboard
     # Set startangle=81 so that segments line up with 20 at the top
-    ax.pie(x=values, labels=labels, colors=colours, startangle=81, textprops={'fontsize':16}) 
+    plt.pie(x=values, labels=labels, colors=colours, startangle=81, textprops={'fontsize':16}) 
     ax.set_title(label='Singles Round the World Performance', fontdict={'fontsize':18, 'weight':'bold'})
-    plt.show()
+
+    plt.savefig('./src/analytics/charts/singles_rtw_heatmap.png')
     
 
 if __name__ == '__main__':
