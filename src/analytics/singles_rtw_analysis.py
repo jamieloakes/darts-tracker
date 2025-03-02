21ba62878eb0af36ce3d047081353d764807e174
jamieloakes <jamieloakeswork@gmail.com>
2025-03-02 15:16:49 +0000

    Finalised dartboard visualisation and added table vis

--- src/analytics/singles_rtw_analysis.py
+++ src/analytics/singles_rtw_analysis.py
@@ -3,22 +3,41 @@ import matplotlib.pyplot as plt
 import src.database as database
 import modules.utils as utils
 
+query:str = """ 
+            SELECT target, SUM(attempts) AS total_attempts 
+            FROM singles_rtw
+            GROUP BY target
+            ORDER BY total_attempts
+            """
+
+
+def showTable():
+    """
+        Show table view of total attempts per target
+    """
+    df:pl.DataFrame = database.queryDatabase(query=query)
+    with pl.Config(tbl_rows=20):
+        print(df)
+
+
 def dartboardVis():
-    query:str = """ 
-                    SELECT target, SUM(attempts) AS total_attempts 
-                    FROM singles_rtw
-                    GROUP BY target
-                """
+    """
+        Show dartboard and colour each target segment based on total attempts\n
+        Darker colour means more attempts at target
+    """
     df:pl.DataFrame = database.queryDatabase(query=query)
 
-    fig:plt.Figure = plt.figure()
-    ax:plt.Axes = fig.add_subplot()
     values:list[int] = [360 / 20 for i in range(20)]
     labels:list[str] = ['S20', 'S5', 'S12', 'S9', 'S14', 'S11', 'S8', 'S16', 'S7', 'S19',
                         'S3', 'S17', 'S2', 'S15', 'S10', 'S6', 'S13', 'S4', 'S18', 'S1']
+    colours:list[tuple[int,int,int]] = utils.conditionalFormatter(df=df, y_col='total_attempts', labels=labels)
     
-    ax.pie(x=values, labels=labels, startangle=81, radius=1.2)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(12,8)
+    ax.pie(x=values, labels=labels, colors=colours, startangle=81, textprops={'fontsize':16})
+    ax.set_title(label='Singles Round the World Performance', fontdict={'fontsize':18, 'weight':'bold'})
     plt.show()
     
+
 if __name__ == '__main__':
     dartboardVis()
