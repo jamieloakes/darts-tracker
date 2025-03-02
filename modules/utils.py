83b7574738f1e4d2d885466c3ad79b34abec36db
jamieloakes <jamieloakeswork@gmail.com>
2025-03-02 20:14:38 +0000

    Removed __init__ file as not needed

--- modules/utils.py
+++ modules/utils.py
@@ -18,11 +18,11 @@ def conditionalFormatter(df:pl.DataFrame, y_col:str, labels:list[str], reverse:b
             labels - Labels to conditionally format for
             reverse - Flag for whether to reverse colours of list
         Returns:
-            colours - RGB value of colour
+            colours - List of RGB values
     """
     # Rank total per target and calculate RGB alpha value for each
     df = df.with_columns((pl.col(y_col).rank('min') / 20).alias('alpha_value'))
-    
+
     # Filter using list comprehension to maintain order relative to labels
     colours:list[tuple[int,int,int,float]] = [(1, 0, 0, df.filter(pl.col('target')==label).select('alpha_value').item()) for label in labels]
 
