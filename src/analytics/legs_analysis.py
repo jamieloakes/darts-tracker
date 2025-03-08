97f21c79ebe81cb4f78119f50ed69935d6c10b64
jamieloakes <jamieloakeswork@gmail.com>
2025-03-08 17:59:42 +0000

    Added time series analysis and refactored in plotly

--- src/analytics/legs_analysis.py
+++ src/analytics/legs_analysis.py
@@ -1,55 +1,116 @@
 import polars as pl
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 
 import src.database as database
 import src.utils as utils
 
-query:str = """
-            SELECT DATE(event_date, 'weekday 0', '-6 day') AS week_commencing,
-            n_darts,
-            avg AS three_dart_avg,
-            ROUND((501.0 / (n_darts - checkout_attempts))*3,2) AS scoring_avg,
-            checkout_attempts,
-            win
-            FROM legs_stats
-            """
+
+def summaryTable() -> None:
+    """ Provide summary stats of 501 performance """
+    query:str = """
+                SELECT n_darts,
+                ROUND((501.0 / (n_darts - checkout_attempts))*3,2) AS scoring_avg,
+                checkout_attempts,
+                win
+                FROM legs_stats
+                """
+    df:pl.DataFrame = database.queryDatabase(query=query)
+
+    aggregated:pl.DataFrame = df.select(
+        pl.count('n_darts').alias('legs'),
+        pl.median('n_darts', 'scoring_avg'),
+        (pl.sum('win') / pl.sum('checkout_attempts')).round(2).alias('checkout_percentage'),
+        (pl.sum('win') / pl.count('n_darts')).round(2).alias('win_percentage')
+    )
+
+    fig = go.Figure(
+         data=[go.Table(
+            header=dict(
+                values=aggregated.columns,
+                line_color='darkslategrey',
+                fill_color='#90E0EF',
+                align='center',
+                font={'size':18, 'color':'black'},
+                height=50
+            ),
+            cells=dict(values=[aggregated['legs'],
+                                aggregated['n_darts'],
+                                aggregated['scoring_avg'],
+                                aggregated['checkout_percentage'],
+                                aggregated['win_percentage']],
+                        line_color='darkslategrey',
+                        fill_color='#CAF0F8',
+                        align='center',
+                        font={'size':16, 'color':'black', 'style':'italic'},
+                        height=30
+            )
+         )]
+    )
+
+    fig.update_layout(width=1200, height=275)
+    fig.write_image('./src/analytics/charts/501_summary_table.png')
 
 
 def timeSeriesAnalysis() -> None:
+    """ Plot median scoring_avg and checkout percentage per week """
+    query:str = """
+                SELECT DATE(event_date, 'weekday 0', '-6 day') AS week_commencing,
+                ROUND((501.0 / (n_darts - checkout_attempts))*3,2) AS scoring_avg,
+                checkout_attempts,
+                win
+                FROM legs_stats
+                """
     df:pl.DataFrame = database.queryDatabase(query=query)
 
     aggregated:pl.DataFrame = df.group_by('week_commencing').agg(
         pl.median('scoring_avg'),
         (pl.sum('win') / pl.sum('checkout_attempts')).round(2).alias('checkout_percentage')
     ).sort(by='week_commencing')
+
+    fig = make_subplots(specs=[[{'secondary_y': True}]])
+
+    fig.add_trace(
+         go.Scatter(x=aggregated.get_column('week_commencing'),
+                    y=aggregated.get_column('scoring_avg'),
+                    mode='lines+markers+text',
+                    line={'dash':'dash'},
+                    name='scoring_avg'),
+         secondary_y=False
+    )
+
+    fig.add_trace(
+         go.Scatter(x=aggregated.get_column('week_commencing'),
+                    y=aggregated.get_column('checkout_percentage'),
+                    mode='lines+markers+text',
+                    name='checkout_percentage'),
+         secondary_y=True
+    )
     
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
+    # Set legend and plot area formatting
+    fig.update_layout(legend={'x':0.5,
+                              'y':1.2,
+                              'orientation':'h',
+                              'xanchor':'center',
+                              'yanchor': 'top',
+                            },
+                        plot_bgcolor='#E1E0E0'
+                    )
 
-    # Scoring average line
-    ax1.plot(aggregated['week_commencing'], aggregated['scoring_avg'], 'g--o', label='scoring_avg')
+    fig.update_xaxes(title_text='week_commencing', type='category', tickangle=-45) # Convert x-axis to categorical to avoid date interpolation
+    fig.update_traces(texttemplate='%{y}', textposition='bottom center') # Add data labels
+
     ax1_top:float = utils.scaleYAxis(x=aggregated.get_column('scoring_avg').max(), base=10)
-    ax1.set_ylim(top=ax1_top, bottom=0)
-    for x,y in zip(aggregated['week_commencing'], aggregated['scoring_avg']):
-        ax1.annotate(text=y, xy=(x,y), textcoords='offset points', xytext=(0,-15), ha='center')
-
-    # Checkout percentage line
-    ax2.plot(aggregated['week_commencing'], aggregated['checkout_percentage'], 'b-o', label='checkout_percentage')
+    fig.update_yaxes(title_text='scoring_avg',
+                     range=[0,ax1_top],
+                     showgrid=False,
+                     secondary_y=False)
+    
     ax2_top:float = utils.scaleYAxis(x=aggregated.get_column('checkout_percentage').max(), base=0.1)
-    ax2.set_ylim(top=ax2_top, bottom=0)
-    for x,y in zip(aggregated['week_commencing'], aggregated['checkout_percentage']):
-            ax2.annotate(text=y, xy=(x,y), textcoords='offset points', xytext=(0,-15), ha='center')
+    fig.update_yaxes(title_text='checkout_percentage',
+                     range=[0,ax2_top],
+                     secondary_y=True)
 
-
-    ax1.set_xlabel('week_commencing')
-    ax1.set_ylabel('scoring_avg', color='g')
-    ax2.set_ylabel('checkout_percentage', color='b')
-
-    fig.legend(loc='upper center')
-    plt.grid(linestyle='--')
-    plt.savefig('./src/analytics/charts/501_timeseries.png', bbox_inches='tight', dpi=160)
-
-
-
-if __name__ == '__main__':
-    timeSeriesAnalysis()
+    fig.write_image('./src/analytics/charts/501_timeseries.png')
