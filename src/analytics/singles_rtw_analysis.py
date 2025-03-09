4b380aea1920c0376a51ece9fe626a19f322c37b
jamieloakes <jamieloakeswork@gmail.com>
2025-03-09 11:07:19 +0000

    Refactored table vis using plotly

--- src/analytics/singles_rtw_analysis.py
+++ src/analytics/singles_rtw_analysis.py
@@ -1,31 +1,50 @@
 import polars as pl
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import numpy as np
 
 import src.database as database
-import src.utils as utils
-
-query:str = """ 
-            SELECT target, SUM(attempts) AS total_attempts 
-            FROM singles_rtw
-            GROUP BY target
-            ORDER BY total_attempts
-            """
 
 
 def attemptsTable():
     """ Create table view of total attempts per target and save as .png """
+    query:str = """
+                SELECT target, attempts
+                FROM singles_rtw
+                """
     df:pl.DataFrame = database.queryDatabase(query=query)
-    fig, ax = plt.subplots()
-    fig.set_size_inches(8,8)
-    ax.axis('off')
+    df = df.group_by('target').agg(
+        pl.sum('attempts').alias('total_attempts'),
+        pl.quantile(column='attempts', quantile=0.25).alias('25th_perc'),
+        pl.median('attempts').alias('median'),
+        pl.quantile(column='attempts', quantile=0.75).alias('75th_perc'),
+    ).sort(by='total_attempts', descending=False)
 
-    table = plt.table(cellText=df.to_pandas(), loc='center', cellLoc='center')
-    table.set_fontsize(size=16)
-    table.scale(1,2)
-
-    plt.savefig('./src/analytics/charts/singles_rtw_table.png')
+    fig = go.Figure(
+        data=[go.Table(
+        header=dict(
+            values=df.columns,
+            line_color='darkslategrey',
+            fill_color='#90E0EF',
+            align='center',
+            font={'size':18, 'color':'black'},
+            height=50
+        ),
+        cells=dict(values=[df['target'],
+                            df['total_attempts'],
+                            df['25th_perc'],
+                            df['median'],
+                            df['75th_perc']
+                        ],
+                    line_color='darkslategrey',
+                    fill_color='#CAF0F8',
+                    align='center',
+                    font={'size':16, 'color':'black', 'style':'italic'},
+                    height=27
+        )
+        )]
+    )
+    fig.update_layout(width=900, height=800)
+    fig.write_image('./src/analytics/charts/singles_rtw_table.png')
 
 
 def dartboardHeatmap() -> None:
@@ -33,6 +52,12 @@ def dartboardHeatmap() -> None:
         Create dartboard visualsation and and save as .png\n
         More vibrant colour means more attempts at target\n
     """
+    query:str = """ 
+            SELECT target, SUM(attempts) AS total_attempts 
+            FROM singles_rtw
+            GROUP BY target
+            ORDER BY total_attempts
+            """
     df:pl.DataFrame = database.queryDatabase(query=query)
     labels:list[str] = ['S20', 'S5', 'S12', 'S9', 'S14', 'S11', 'S8', 'S16', 'S7', 'S19',
                         'S3', 'S17', 'S2', 'S15', 'S10', 'S6', 'S13', 'S4', 'S18', 'S1']
@@ -62,9 +87,4 @@ def dartboardHeatmap() -> None:
         }
         },
     )
-
     fig.write_image('./src/analytics/charts/singles_rtw_heatmap.png')
-    
-
-if __name__ == '__main__':
-    dartboardHeatmap()
