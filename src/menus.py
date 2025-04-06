04bba1002cb634d0c2b12413ca3cec7e3ae0bb92
jamieloakes <jamieloakeswork@gmail.com>
2025-04-06 12:56:04 +0100

    Modified error handling

--- src/menus.py
+++ src/menus.py
@@ -1,6 +1,7 @@
+import uuid
+
 import src.utils as utils
 import src.database as database
-import uuid
 
 def printOptionMenu() -> None:
     """ Display option screen for practice games """
@@ -16,19 +17,22 @@ def printOptionMenu() -> None:
 def singlesRtwMenu() -> None:
     """ Data capture menu for Round the World (Singles) practice game """
     today:str = utils.getTodaysDate()
-    data:list[tuple[str, str, int]] = []
+    data:list[tuple[str, str, str, int]] = []
     game_id:str = str(uuid.uuid4)
 
     target_count:int = 1
     while target_count < 21:
         try:
             target:str = f'S{target_count}'
-            attempts:int = int(input(f'{target}: '))
+            attempts:int = int(input(f'{target}: ')); assert attempts > 0
             data.append((game_id, today, target, attempts))
             target_count += 1
         except ValueError:
             print('Invalid input. Please try again.')
-            continue
+        except AssertionError:
+            print('Value must be greater than 0')
+        except KeyboardInterrupt:
+            break
     database.insertRecords(table='singles_rtw', fields=('game_id', 'event_date', 'target', 'attempts'), data=data)
     print()
 
@@ -36,21 +40,22 @@ def singlesRtwMenu() -> None:
 def doublesRtwMenu() -> None:
     """ Data capture menu for Round the World (Doubles) practice game """
     today:str = utils.getTodaysDate()
-    data:list[tuple[str, str, int]] = []
+    data:list[tuple[str, str, str, int]] = []
     game_id:str = str(uuid.uuid4)
 
     target_count:int = 1
     while target_count < 21:
         try:
             target:str = f'D{target_count}'
-            hits:int = int(input(f'{target}: '))
-            if hits > 3:
-                raise ValueError()
+            hits:int = int(input(f'{target}: ')); assert -1 < hits < 4
             data.append((game_id, today, target, hits))
             target_count += 1
         except ValueError:
             print('Invalid input. Please try again.')
-            continue
+        except AssertionError:
+            print('Value must be between 0 and 3')
+        except KeyboardInterrupt:
+            break
     database.insertRecords(table='doubles_rtw', fields=('game_id', 'event_date', 'target', 'hits'), data=data)
     print()
 
@@ -66,13 +71,14 @@ def legsStatsMenu() -> None:
             n_darts:int = int(input('n_darts: '))
             avg:float = float(input('avg: '))
             checkout_attempts:int = int(input('checkout_attempts: '))
-            win:int = int(input('win: '))
-            if (win != 0) and (win != 1):
-                raise ValueError()
+            win:int = int(input('win: ')); assert -1 < win < 2
             data.append((today, n_darts, avg, checkout_attempts, win))
             completed = True
         except ValueError:
             print('Invalid input. Please try again.')
-            continue
+        except AssertionError:
+            print('Win field must be 0 (Loss) or 1 (Win)')
+        except KeyboardInterrupt:
+            break
     database.insertRecords(table='legs_stats', fields=('event_date', 'n_darts', 'avg', 'checkout_attempts', 'win'), data=data)
     print()
